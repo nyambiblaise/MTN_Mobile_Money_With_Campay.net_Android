30f4463426ff23dec32ec8c796438474af2ed09e
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-07 01:59:51 +0100

    g

--- app/src/main/java/com/compay/momo/MainActivity.java
+++ app/src/main/java/com/compay/momo/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     private CamPay camPay;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
