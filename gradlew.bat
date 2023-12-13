284fd3a936175d4a2a43bc39d9f30089570ef5eb
Nyambi Blaise - Dəv / 237 <34681563+nyambiblaise@users.noreply.github.com>
2023-12-13 00:10:42 -0500

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -5,7 +5,6 @@
 @rem  Gradle startup script for Windows. 
 @rem
 @rem ##########################################################################
-
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
